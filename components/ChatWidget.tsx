52cc45b6b5685e4e22872c6c8f2e1acf3e25e648
Pooja-p15 <55484038+Pooja-p15@users.noreply.github.com>
2026-04-17 10:58:05 +0530

    Update ChatWidget.tsx

--- components/ChatWidget.tsx
+++ components/ChatWidget.tsx
@@ -30,11 +30,11 @@ export default function ChatWidget() {
 
                 embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
                 embeddedservice_bootstrap.init(
-                    '00Dg5000002kBx3',
-                    'Web_Agent',
-                    'https://orgfarm-d4d03d3f06-dev-ed.develop.my.site.com/ESWWebAgent1767442807450',
+                    '00DdM00000sybdV',
+                    'Salesforcetroop_Ai_Web',
+                    'https://orgfarm-98e1e00b52-dev-ed.develop.my.site.com/ESWSalesforcetroopAiWeb1776402731780',
                     {
-                        scrt2URL: 'https://orgfarm-d4d03d3f06-dev-ed.develop.my.salesforce-scrt.com'
+                        scrt2URL: 'https://orgfarm-98e1e00b52-dev-ed.develop.my.salesforce-scrt.com'
                     }
                 );
               } catch (err) {
@@ -46,7 +46,7 @@ export default function ChatWidget() {
       />
 
       <Script
-        src="https://orgfarm-d4d03d3f06-dev-ed.develop.my.site.com/ESWWebAgent1767442807450/assets/js/bootstrap.min.js"
+        src="https://orgfarm-98e1e00b52-dev-ed.develop.my.site.com/ESWSalesforcetroopAiWeb1776402731780/assets/js/bootstrap.min.js"
         strategy="afterInteractive"
         onLoad={() => {
           if (typeof window !== 'undefined' && typeof window.initEmbeddedMessaging === 'function') {
